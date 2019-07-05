78002c13f2eb9c7bd6a5d92a158212e216492f2f
Pamela-C-Torres-Gtz <pamelatgtz@gmail.com>
2019-07-05 13:00:50 -0500

    Aparece data en tarjetas, funcion de ordenar en data.js.(PERo falta que ejecute). Estilo de letra y tarjetas en css

--- src/data.js
+++ src/data.js
@@ -1,72 +1,44 @@
+//funcion ordenar de ordenar
 window.pokemongo = {
-  filterData(data, condition)  {
-    let tiposDePokemon;
-    if (typeof condition === "str") 
-    {
-      tiposDePokemon = data.filter(tipos =>{
-        return tipos.type.indexOf(condition) !== -1;
-       })
-          
-    }
-    else {
-      tiposDePokemon = data.filter(candy =>{
-        return candy.candy_count===(condition);
-      })  
-  }
-  return tiposDePokemon; 
-}
-};
-
-
-
-  /*
- window.pokemongoOrdenar = {
-
-  sortData: (data, sortBy, sortOrder) => {
+  sortData: (data, sortBy, sortOrder) => { //Ordenar por alfabeto de menor a mayo
     if (sortBy === "name" && sortOrder === "a-z"){
       data.sort((a,b) =>{
-        if (a.name < b.name) {
-          return -1;
-        }
         if (a.name > b.name) {
           return 1;
         }
-        //a debe ser igual b
+        if (a.name < b.name) {
+          return -1;
+        }
         return 0;
       } )
      return data;
 
-    } 
+    }
 
     if (sortBy === "name" && sortOrder === "z-a"){
       data.sort((a,b) =>{
-        if (a.name > b.name) {
-          return -1;
-        }
         if (a.name < b.name) {
           return 1;
         }
-        //a debe ser igual b
+        if (a.name > b.name) {
+          return -1;
+        }
         return 0;
-        
       } )
-     return data;
-
-    } 
-    
+     return data
+    }
 
     if (sortBy === "number" && sortOrder === "1-151"){
       data.sort((a,b) =>{
-        if (a.id < b.id) {
-          return -1;
-        }
         if (a.id > b.id) {
           return 1;
         }
-        //a debe ser igual b
-        return 0;
-        
+        if (a.id < b.id) {
+          return -1;
+        }
+        return 0; 
       } )
      return data;
   }
-},*/
+  }
+}
